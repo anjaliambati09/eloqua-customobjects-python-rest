228c0947d4ce792eb049f973a9efae484c7216ff
anjaliambati09 <65726235+anjaliambati09@users.noreply.github.com>
2020-05-26 03:05:15 +0400

    Update get_customObjects.py

--- get_customObjects.py
+++ get_customObjects.py
@@ -1,7 +1,10 @@
 import sys
+import json
 sys.path.append('./lib')
 from eloqua_request import EloquaRequest
 
 request = EloquaRequest('site', 'user', 'password')
-response = request.get('assets/customObjects', None)
-print(response)
+response = request.get('/assets/customObjects', None)
+#print(response)
+JSON_object = json.loads(response.decode('utf-8'))
+print(JSON_object)
