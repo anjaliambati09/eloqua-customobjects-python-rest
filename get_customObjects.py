442a49a9b16eb376a22f1297d30372ca20d2c50a
anjaliambati09 <65726235+anjaliambati09@users.noreply.github.com>
2020-05-25 18:29:05 +0400

    Update get_customObjects.py

--- get_customObjects.py
+++ get_customObjects.py
@@ -2,5 +2,6 @@ import sys
 sys.path.append('./lib')
 from eloqua_request import EloquaRequest
 
-request = EloquaRequest('site', 'user', 'password')
-response = request.get('/data/customObject/{id}', None)
+request = EloquaRequest('BroadridgeFinancialSolutionsInc', 'anjali.ambati', 'Ads@0220')
+response = request.get('assets/customObjects', None)
+print response
